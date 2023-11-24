5d45cd8c60d6a7fb022a0aea341d55a754fb6d6f
Edeok <carosartini.cs@gmail.com>
2023-11-24 13:26:43 -0300

    URL-IPFS

--- src/pages/imageLoad.js
+++ src/pages/imageLoad.js
@@ -1,36 +1,44 @@
 import axios from 'axios';
 
-const url = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
+const uploadUrl = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
 
 async function uploadFile(file) {
- const data = new FormData();
- data.append('file', file);
+  const data = new FormData();
+  data.append('file', file);
 
- const headers = {
+  const headers = {
     'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-    'pinata_api_key': '96bcb01cab23ce55c883',
-    'pinata_secret_api_key': '71501b126177b87aaa7b5bab28b725ac5c1ea8587595c19b986098482ac0d051',
- };
+    'pinata_api_key': 'fe3898969c15e7f996c5',
+    'pinata_secret_api_key': '719d3ad6d2ec8efbe51c73cb26df781f13b6b2bd088c881ea6039d01174f6a39'
+  };
 
- try {
-    const response = await axios.post(url, data, { headers });
-    console.log('File uploaded successfully. CID:', response.data.IpfsHash);
- } catch (error) {
+  try {
+    const response = await axios.post(uploadUrl, data, { headers });
+    const cid = response.data.IpfsHash;
+    const ipfsGateway = 'https://ipfs.io/ipfs/';
+    const imageUrl = ipfsGateway + cid; 
+    
+    console.log('File uploaded successfully. CID:', cid);
+    console.log('IPFS URL:', imageUrl);
+
+    return { cid, url: imageUrl };
+  } catch (error) {
     console.error('Error uploading file:', error);
- }
+    return { error };
+  }
 }
 
 function ImageLoad() {
- const handleFileUpload = async (e) => {
+  const handleFileUpload = async (e) => {
     const file = e.target.files[0];
     await uploadFile(file);
- };
+  };
 
- return (
+  return (
     <div>
       <input type="file" onChange={handleFileUpload} />
     </div>
- );
+  );
 }
 
 export default ImageLoad;
