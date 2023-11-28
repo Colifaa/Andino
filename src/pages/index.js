15b16f2d015794ec1880f112d1536b5736e38e70
lucasp149 <lucasp149@gmail.com>
2023-11-28 20:23:21 -0300

    added the mint page

--- src/pages/index.js
+++ src/pages/index.js
@@ -952,182 +952,178 @@ const address = '0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47';
 
 
 export default function CreatePoaps() {
-  const [url, setUrl] = useState("");
-  const [eventTitle, setEventTitle] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [eventNum, setEventNum] = useState("");
-  const [eventDescription, setEventDescription] = useState("");
+	const [url, setUrl] = useState("");
+	const [eventTitle, setEventTitle] = useState("");
+	const [startDate, setStartDate] = useState("");
+	const [endDate, setEndDate] = useState("");
+	const [eventNum, setEventNum] = useState("");
+	const [eventDescription, setEventDescription] = useState("");
 
-  const [eventId, setEventId] = useState("");
+	const [eventId, setEventId] = useState("");
 
-  const [imageUrl, setImageUrl] = useState('');
- 
-
-  
-
-  async function createEvent(e) {
-    e.preventDefault();
-  
-    // Obtén la fecha actual con zona horaria
-    const currentDate = new Date();
-    const currentDateInSeconds = Math.floor(currentDate.getTime() / 1000);
-  
-    if (typeof window.ethereum !== "undefined") {
-      const web3 = new Web3(window.ethereum);
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-  
-      const accounts = await web3.eth.getAccounts();
-      const contract = new web3.eth.Contract(abi, address);
-  
-      // Asegúrate de que las fechas se estén configurando correctamente
-      let startDateInSeconds = (new Date(startDate).getTime() / 1000).toString();
-      let expirationDateInSeconds = (new Date(endDate).getTime() / 1000).toString();
-  
-      console.log("startDate", startDateInSeconds);
-      console.log("expirationDate", expirationDateInSeconds);
-  
-      if (expirationDateInSeconds < startDateInSeconds) {
-        console.error("La fecha de finalización debe ser en el futuro");
-        return;
-      }
-
-      try {
-        const result = await contract.methods.createPoap(
-          eventTitle,
-          currentDateInSeconds,
-          startDateInSeconds,
-          expirationDateInSeconds,
-          eventNum,
-          eventId // Agrega el nuevo campo del ID
-        ).send({ from: accounts[0] });
-  
-        console.log("Evento creado:", result);
-        // Aquí puedes actualizar el estado 'url' con la URL generada
-
-        const CID = result.cid;
-
-        const generatedUrl = `https://ipfs.io/ipfs/${CID}`; // ¡Asegúrate de ajustar la lógica para obtener la URL correcta!
-
-        console.log("Evento creado:", result);
-        setUrl(generatedUrl); // Establece la URL generada en el estado 'url'
-        // setUrl(generatedUrl);
-      } catch (error) {
-        console.error("Error al crear el evento:", error);
-      }
-    } else {
-      console.error("Web3 no está disponible en este navegador");
-}
-}
+	const [imageUrl, setImageUrl] = useState('');
 
 
-  return (
-	<div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"
-	style={{ backgroundImage: `url('https://i.ibb.co/HxcGFCN/R.jpg')`, backgroundSize: 'cover' }}>
-		  
-		  
-    
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
-          <div className="max-w-md mx-auto">
-            <div className="flex items-center space-x-5">
-              <div className="h-14 w-14 bg-yellow-200 rounded-full flex flex-shrink-0 justify-center items-center text-yellow-500 text-2xl font-mono">i</div>
-              <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
-                <h2 className="leading-relaxed">Create an Event</h2>
-                <p className="text-sm text-gray-500 font-normal leading-relaxed">"POAP Odyssey: Donde las Ideas se Transforman en Asombrosos Poaps NFT"</p>
-              </div>
-            </div>
-            <form onSubmit={createEvent}>
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div className="flex flex-col">
-                  <label className="leading-loose">Event Title</label>
-                  <input
-                    type="text"
-                    className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                    placeholder="Event title"
-                    value={eventTitle}
-                    onChange={(e) => setEventTitle(e.target.value)}
-                  />
-                </div>
-                <div className="flex items-center space-x-4">
-  <div className="flex flex-col">
-    <label className="leading-loose">Start</label>
-    <div className="relative focus-within:text-gray-600 text-gray-400">
-      <input
-        type="date"
-        className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-        value={startDate}
-        onChange={(e) => setStartDate(e.target.value)}
-      />
-      <div className="absolute left-3 top-2">
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-      </div>
-    </div>
-  </div>
-  <div className="flex flex-col">
-    <label className="leading-loose">End</label>
-    <div className="relative focus-within:text-gray-600 text-gray-400">
-      <input
-        type="date"
-        className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-        value={endDate}
-        onChange={(e) => setEndDate(e.target.value)}
-      />
-      <div className="absolute left-3 top-2">
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-      </div>
-    </div>
-  </div>
-</div>
 
-                <div className="flex flex-col">
-                  <label className="leading-loose">Quantity of Poaps</label>
-                  <input
-                    type="text"
-                    className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                    placeholder="Cantidad de Poaps"
-                   
-                    value={eventNum}
-                    onChange={(e) => setEventNum(e.target.value)}
-                  />
-                </div>
-                
-              <div className="flex flex-col">
-  <label className="leading-loose">Event Description</label>
-  <textarea
-  
-    className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-    placeholder="Descripción del evento"
-    value={eventDescription}
-    onChange={(e) => setEventDescription(e.target.value)}
-  />
-</div>
-              </div>
-        
 
-          <ImageLoad />
+	async function createEvent(e) {
+		e.preventDefault();
 
-           
+		// Obtén la fecha actual con zona horaria
+		const currentDate = new Date();
+		const currentDateInSeconds = Math.floor(currentDate.getTime() / 1000);
 
-            
-              <div className="pt-4 flex items-center space-x-4">
-                <button
-                  className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none"
-                  type="submit"
-                >
-                  Create
-                </button>
+		if (typeof window.ethereum !== "undefined") {
+			const web3 = new Web3(window.ethereum);
+			await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-             
-              </div>
-    
-              
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-	
-  );
-  
+			const accounts = await web3.eth.getAccounts();
+			const contract = new web3.eth.Contract(abi, address);
+
+			// Asegúrate de que las fechas se estén configurando correctamente
+			let startDateInSeconds = (new Date(startDate).getTime() / 1000).toString();
+			let expirationDateInSeconds = (new Date(endDate).getTime() / 1000).toString();
+
+			console.log("startDate", startDateInSeconds);
+			console.log("expirationDate", expirationDateInSeconds);
+
+			if (expirationDateInSeconds < startDateInSeconds) {
+				console.error("La fecha de finalización debe ser en el futuro");
+				return;
+			}
+
+			try {
+				const result = await contract.methods.createPoap(
+					eventTitle,
+					currentDateInSeconds,
+					startDateInSeconds,
+					expirationDateInSeconds,
+					eventNum,
+					eventId // Agrega el nuevo campo del ID
+				).send({ from: accounts[0] });
+
+				console.log("Evento creado:", result);
+				// Aquí puedes actualizar el estado 'url' con la URL generada
+
+				const CID = result.cid;
+
+				const generatedUrl = `https://ipfs.io/ipfs/${CID}`; // ¡Asegúrate de ajustar la lógica para obtener la URL correcta!
+
+				console.log("Evento creado:", result);
+				setUrl(generatedUrl); // Establece la URL generada en el estado 'url'
+				// setUrl(generatedUrl);
+			} catch (error) {
+				console.error("Error al crear el evento:", error);
+			}
+		} else {
+			console.error("Web3 no está disponible en este navegador");
+		}
+	}
+
+
+	return (
+		<div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"
+			style={{ backgroundImage: `url('https://i.ibb.co/HxcGFCN/R.jpg')`, backgroundSize: 'cover' }}>
+
+
+
+			<div className="relative py-3 sm:max-w-xl sm:mx-auto">
+				<div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
+					<div className="max-w-md mx-auto">
+						<div className="flex items-center space-x-5">
+							<div className="h-14 w-14 bg-yellow-200 rounded-full flex flex-shrink-0 justify-center items-center text-yellow-500 text-2xl font-mono">i</div>
+							<div className="block pl-2 font-semibold text-xl self-start text-gray-700">
+								<h2 className="leading-relaxed">Create an Event</h2>
+								<p className="text-sm text-gray-500 font-normal leading-relaxed">"POAP Odyssey: Donde las Ideas se Transforman en Asombrosos Poaps NFT"</p>
+							</div>
+						</div>
+						<form onSubmit={createEvent}>
+							<div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+								<div className="flex flex-col">
+									<label className="leading-loose">Event Title</label>
+									<input
+										type="text"
+										className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+										placeholder="Event title"
+										value={eventTitle}
+										onChange={(e) => setEventTitle(e.target.value)}
+									/>
+								</div>
+								<div className="flex items-center space-x-4">
+									<div className="flex flex-col">
+										<label className="leading-loose">Start</label>
+										<div className="relative focus-within:text-gray-600 text-gray-400">
+											<input
+												type="date"
+												className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+												value={startDate}
+												onChange={(e) => setStartDate(e.target.value)}
+											/>
+											<div className="absolute left-3 top-2">
+												<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+											</div>
+										</div>
+									</div>
+									<div className="flex flex-col">
+										<label className="leading-loose">End</label>
+										<div className="relative focus-within:text-gray-600 text-gray-400">
+											<input
+												type="date"
+												className="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+												value={endDate}
+												onChange={(e) => setEndDate(e.target.value)}
+											/>
+											<div className="absolute left-3 top-2">
+												<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="flex flex-col">
+									<label className="leading-loose">Quantity of Poaps</label>
+									<input
+										type="text"
+										className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+										placeholder="Cantidad de Poaps"
+
+										value={eventNum}
+										onChange={(e) => setEventNum(e.target.value)}
+									/>
+								</div>
+
+								<div className="flex flex-col">
+									<label className="leading-loose">Event Description</label>
+									<textarea
+
+										className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+										placeholder="Descripción del evento"
+										value={eventDescription}
+										onChange={(e) => setEventDescription(e.target.value)}
+									/>
+								</div>
+							</div>
+
+							<ImageLoad />
+
+							<div className="pt-4 flex items-center space-x-4">
+								<button
+									className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none"
+									type="submit"
+								>
+									Create
+								</button>
+
+
+							</div>
+
+
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	);
+
 }
