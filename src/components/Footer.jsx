2c627dcfcc5424dfe08741bc84914729c22a34bb
unknown <Jorgemathez14@gmail.com>
2023-11-29 21:23:53 -0300

    styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="relative bg-blueGray-200 pt-8 pb-6">
+    <footer className="relative dark:bg-[#0c1015] pt-8 pb-6">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h4 className="text-3xl font-semibold text-blueGray-700">Let's keep in touch!</h4>
-            <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+            <h4 className="text-3xl font-semibold text-blue-800 ">Let's keep in touch!</h4>
+            <h5 className="text-[2rem] leading-[3rem] tracking-tight font-bold text-black dark:text-white">
               Find us on any of these platforms, we respond 1-2 business days.
             </h5>
             <div className="mt-6 lg:mb-0 mb-6">
@@ -29,11 +29,11 @@ const Footer = () => {
         <hr className="my-6 border-blueGray-300" />
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-            <div className="text-sm text-blueGray-500 font-semibold py-1">
+            <div className="text-sm text-blue-800 font-semibold py-1">
               Copyright © <span id="get-current-year">2021</span>
               <a
                 href="https://www.creative-tim.com/product/notus-js"
-                className="text-blueGray-500 hover:text-gray-800"
+                className="text-blue-800 hover:text-gray-800"
                 target="_blank"
                 rel="noopener noreferrer"
               >
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider >
+  <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
