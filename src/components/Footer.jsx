4913a7664076d51c72f0a2f62827b577848f1615
unknown <Jorgemathez14@gmail.com>
2023-12-19 15:59:04 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h4 className="text-3xl font-semibold text-blue-800 ">Let's keep in touch!</h4>
+            <h4 className="text-3xl font-semibold text-blue-800 ">POAPs Generated</h4>
             <h5 className="text-[2rem] leading-[3rem] tracking-tight font-bold text-black dark:text-white">
-              Find us on any of these platforms, we respond 1-2 business days.
+            Únase a nosotros en las redes sociales para conocer las últimas actualizaciones sobre la creación y acuñación de Poap.
             </h5>
             <div className="mt-6 lg:mb-0 mb-6">
               <button
@@ -30,14 +30,14 @@ const Footer = () => {
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-blue-800 font-semibold py-1">
-              Copyright © <span id="get-current-year">2021</span>
+              © {new Date().getFullYear()} Your Company Name. All rights reserved.
               <a
-                href="https://www.creative-tim.com/product/notus-js"
+                href="https://www.example.com"
                 className="text-blue-800 hover:text-gray-800"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Notus JS by Creative Tim.
+                Visite nuestro sitio web
               </a>
             </div>
           </div>
