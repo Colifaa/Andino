cb4b9c56d3ad9dc1cb3d78c91892edf79db192dc
unknown <Jorgemathez14@gmail.com>
2023-12-19 18:43:46 -0300

    muchos estilos

---
+++ src/components/NavBarUser.jsx
@@ -0,0 +1,34 @@
+import { Flex, Box, Image } from '@chakra-ui/react';
+import Link from 'next/link';
+
+const NavBarUser = () => {
+  return (
+    <Box bg="white" boxShadow="md">
+      <nav className="fixed inset-x-0 top-0 z-10 w-full px-4 py-1 bg-[#YourPOAPColor] shadow-md border-[#YourBorderColor] dark:bg-[#YourDarkColor] transition duration-700 ease-out">
+        <Flex justify="space-between" align="center" p={4}>
+          <Link href="/mint/1">
+            <Box width="8rem" height="auto">
+              <Image
+                src="https://th.bing.com/th/id/OIG.TXoN1fY_SxiGAMzB2QF.?pid=ImgGn"
+                alt="POAP Logo"
+                borderRadius="full"
+                boxSize="70px"
+              />
+            </Box>
+          </Link>
+          <div className="flex items-center space-x-4 text-lg font-semibold tracking-tight">
+            <Link
+              
+              className=" px-6 py-2 text-white transition duration-500 ease-out bg-black bg-opacity-50 border border-black rounded-lg hover:bg-opacity-100"
+              href="/collection"
+            >
+       Mis POAPs
+            </Link>
+          </div>
+        </Flex>
+      </nav>
+    </Box>
+  );
+};
+
+export default NavBarUser;
